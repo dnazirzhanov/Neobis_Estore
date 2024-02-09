b8b0a737329a1860fe5f5e2ccaaf2f6e2192b090
Dastan Nazirzhanov <dnazirzhonov@gmail.com>
2024-02-09 16:31:36 +0600

    Register models to admin

--- mall_of_america/admin.py
+++ mall_of_america/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Product, Category, Order, Comment
 
-# Register your models here.
+
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Order)
+admin.site.register(Comment)
+
+
+
+
