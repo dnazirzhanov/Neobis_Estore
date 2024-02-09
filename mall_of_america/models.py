637c3f5f475e7efbb9cb1532bf1ed7056137d9c9
Dastan Nazirzhanov <dnazirzhonov@gmail.com>
2024-02-09 16:28:01 +0600

    Create the following models : Product, Category, Order, Comment

--- mall_of_america/models.py
+++ mall_of_america/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    cat = models.ForeignKey(Category, on_delete=models.PROTECT)
+    sale = models.IntegerField()
 
-# Create your models here.
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    time_created = models.DateTimeField(auto_now_add=True)
+    address = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Order #{self.id} made by {self.user.username}"
+
+class Comment(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    comment = models.TextField()
+    time_comment_created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"User - {self.user} commented the product - {self.prduct.name}"
+
+
+
