ef7fbe52e5c71edddd75b166a564b82515fbb98f
Dastan Nazirzhanov <dnazirzhonov@gmail.com>
2024-02-10 12:12:44 +0600

    Add a permission functionality

--- mall_of_america/views.py
+++ mall_of_america/views.py
@@ -7,16 +7,21 @@ from .serializers import ProductSerializer, OrderSerializer, CategorySerializer,
 class ProductAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated]
 
 class OrderProduct(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
 
 class MakeComment(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
 
